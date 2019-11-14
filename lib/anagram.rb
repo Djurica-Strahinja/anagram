5538ba847161f335d6eabf6a6ccf718e4e2efc21
Strahinja <djuricastrahinja@gmail.com>
2019-11-14 02:03:57 +0100

    Remove unnecessary variables

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,32 +1,25 @@
 class Anagram
   def initialize(sentence1, sentence2)
-    @sentence1 = sentence1
-    @sentence2 = sentence2
+    @sentence1 = sentence1.downcase
+    @sentence2 = sentence2.downcase
   end
 
   def is_anagram
     if @sentence1 == "" or @sentence2 == ""
       return "One or two arguments are empty, please write 2 sentences!"
-    elsif @sentence1.is_a? Numeric or @sentence2.is_a? Numeric
-      return "Numbers are not allowed"
     else
       sen_1_char = ""
       sen_2_char = ""
-
       @sentence1.each_char do |char|
-        if ("A".."Z").include? char or ("a".."z").include? char
+        if ("a".."z").include? char and char !=" "
           sen_1_char += char
         end
       end
       @sentence2.each_char do |char|
-        if ("A".."Z").include? char or ("a".."z").include? char
+        if ("a".."z").include? char and char != " "
           sen_2_char += char
         end
       end
-
-      sen_1_char = sen_1_char.downcase.delete(' ')
-      sen_2_char = sen_2_char.downcase.delete(' ')
-
       if sen_1_char.chars.sort == sen_2_char.chars.sort
         true
       else
