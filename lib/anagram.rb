a4fa7d17306a394d4362b9eedde84e82c0c79acc
Strahinja <djuricastrahinja@gmail.com>
2019-11-20 12:22:15 +0100

    Remove instance variables

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,16 @@
 class Anagram
-  def initialize(sentence1, sentence2)
-    @sentence1 = sentence1.downcase
-    @sentence2 = sentence2.downcase
-  end
-
-  def is_anagram
-    if @sentence1 == "" or @sentence2 == ""
+  def is_anagram(sentence1,sentence2)
+    if sentence1 == "" or sentence2 == ""
       return "One or two arguments are empty, please write 2 sentences!"
     else
       sen_1_char = ""
       sen_2_char = ""
-      @sentence1.each_char do |char|
+      sentence1.each_char do |char|
         if ("a".."z").include? char and char !=" "
           sen_1_char += char
         end
       end
-      @sentence2.each_char do |char|
+      sentence2.each_char do |char|
         if ("a".."z").include? char and char != " "
           sen_2_char += char
         end
